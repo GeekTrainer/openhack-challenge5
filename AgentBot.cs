736a5b4a56eac6664499b0501944ddceeb634993
Christopher Harrison <chrhar@microsoft.com>
2018-05-31 10:57:09 -0700

    walkting through challenge 5 still

--- AgentBot.cs
+++ AgentBot.cs
@@ -7,9 +7,26 @@ using Microsoft.Bot.Schema;
 namespace auth_test {
     public class AgentBot : IBot {
         public async Task OnTurn(ITurnContext context) {
-            if(context.Activity.Type != ActivityTypes.Message) return;
-
-            await context.SendActivity("Hi there! I'm a sample echo bot");
+            if(context.Activity.Type != ActivityTypes.Message) {
+                await context.SendActivity("Hi there! I'm a sample echo bot");
+            } else if(context.Activity.Type == ActivityTypes.ConversationUpdate) {
+                await context.SendActivity(new Activity() {
+                    Type = "event",
+                    Text = context.Activity.From.Id
+                });
+            } else if(context.Activity.Type == "event" && context.Activity.Text == "authenticated") {
+                // authentication has taken place
+                // check the state of the user and see if it's been updated
+                var state = context.GetConversationState<AuthenticationState>();
+                if(state.Authenticated) {
+                    await context.SendActivity("You have been authenticated");
+                }
+            }
         }
+    }
+
+    public class AuthenticationState
+    {
+        public bool Authenticated { get; set; }
     }
 }
---
+++ Controllers/AgentAuthentiactionController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using auth_test.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+
+namespace auth_test.Controllers
+{
+    public class AgentAuthenticationController : Controller
+    {
+        [Route("/api/agentauthentication/authenticate")]
+        [Authorize()]
+        public async Task<IActionResult> AuthenticateAsync(string id, IConfiguration configuration)
+        {
+            // create an adapter and context to load the state
+            var adapter = new Microsoft.Bot.Builder.Adapters.BotFrameworkAdapter(new ConfigurationCredentialProvider(configuration));
+            var context = new TurnContext(adapter, new Activity() {
+                From = new ChannelAccount(id),
+                Type = "event",
+                Text = "authenticated"
+            });
+
+            // load the state and set the Authenticated property
+            var state = context.GetConversationState<AuthenticationState>();
+            state.Authenticated = true;
+            
+            // Message the bot that authentication has taken place
+            var bot = new AgentBot();
+            await bot.OnTurn(context);
+            return Ok();
+        }
+    }
+}
