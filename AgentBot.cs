275f03520174c89ee041f58039bc226617840572
Christopher Harrison <chrhar@microsoft.com>
2018-05-31 15:32:19 -0700

    Registered handler for backchannel

--- AgentBot.cs
+++ AgentBot.cs
@@ -7,14 +7,15 @@ using Microsoft.Bot.Schema;
 namespace auth_test {
     public class AgentBot : IBot {
         public async Task OnTurn(ITurnContext context) {
-            if(context.Activity.Type != ActivityTypes.Message) {
+            if(context.Activity.Type == ActivityTypes.Message) {
                 var state = context.GetConversationState<AuthenticationState>();
                 if(state.Authenticated) await context.SendActivity("You are logged in!!");
                 else await context.SendActivity("You are not logged in");
             } else if(context.Activity.Type == ActivityTypes.ConversationUpdate) {
                 await context.SendActivity(new Activity() {
                     Type = "event",
-                    Text = context.Activity.From.Id
+                    Text = context.Activity.From.Id,
+                    Name = "authenticate"
                 });
             } else if(context.Activity.Type == "event" && context.Activity.Text == "authenticated") {
                 // authentication has taken place
